77f05e03affb28b41dd29baa8d5e527af361ca46
unknown <mohammed_ahmed4949@yahoo.com>
2019-07-01 00:12:32 +0200

    Edited in createProject

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -9,7 +9,6 @@ export class CreateProject extends Component {
     };
     handleSubmit = (e) => {
         e.preventDefault();
-        //console.log(this.state);
         this.props.createProject(this.state);
     }
     handleChange = (e) => {
