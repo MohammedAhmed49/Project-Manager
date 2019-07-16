ae3bfe182c7df81456a518632132be2c504d65dd
momo_ahmed <mahmed@icon-creations.com>
2019-07-16 17:28:44 +0200

    Add new document to projects collection firebase functionality

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,6 +10,15 @@ const projectReducer = (state = initState, action) => {
   switch(action.type){
     case 'CREATE_PROJECT':
       console.log(action.project);
+      state = {
+        projects: [...initState.projects, action.project]
+      }
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+        console.log(action.err);
+        return state;
+    default:
+        return state;
   }
   return state;
 }
